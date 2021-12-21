b07a256247176a23040ea1db4ea016fd2ddfc467
system <system@cubecloud.dev>
2021-12-21 09:43:15 +0000

    test

--- cube.js
+++ cube.js
@@ -2,7 +2,7 @@ module.exports = {
   http: {
     cors: {
       origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       preflightContinue: false,
       optionsSuccessStatus: 204,
       maxAge: 86400,
